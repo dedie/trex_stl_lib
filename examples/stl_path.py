abdf95df6226f4d257697aec071c99d8f886ba6a
Daron Andrew Edie <dedie@superloop.com>
2019-01-08 09:46:27 +1000

    Malformed path

--- examples/stl_path.py
+++ examples/stl_path.py
@@ -5,4 +5,4 @@ cur_dir = os.path.dirname(__file__)
 
 # Run from TREX_STL_LIB directory
 sys.path.insert(0, "../")
-STL_PROFILES_PATH = cur_dir + 'profiles/stl/'
+STL_PROFILES_PATH = cur_dir + '/profiles/stl/'
