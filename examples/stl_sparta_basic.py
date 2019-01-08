f63b3c7f17584c77ab8294a667b85d100320e2fa
Daron Andrew Edie <dedie@superloop.com>
2019-01-08 15:01:53 +1000

    Added print out of connection info for debugging

--- examples/stl_sparta_basic.py
+++ examples/stl_sparta_basic.py
@@ -13,29 +13,34 @@ from trex_stl_std import stl_map_ports
 from trex_stl_streams import STLProfile
 
 
-# IMIX test
+# Basic sparta test
 # it maps the ports to sides
-# then it load a predefind profile 'IMIX'
+# then it load a predefind profile 'sparta_basic'
 # and attach it to both sides and inject
 # at a certain rate for some time
 # finally it checks that all packets arrived
-def imix_test(server, mult):
+def sparta_basic_test(server, mult):
 
     # create client
-    c = STLClient(server=server)
+    client = STLClient(server=server)
 
     passed = True
 
     try:
 
         # connect to server
-        c.connect()
+        client.connect()
 
         # take all the ports
-        c.reset()
+        client.reset()
+        print('Connection info:\n')
+        print(client.get_connection_info() + '\n\n')
 
         # map ports - identify the routes
-        table = stl_map_ports(c)
+        table = stl_map_ports(client)
+        print('\n\n')
+        print('Actual mapped table(all data):\n')
+        print(table + '\n')
 
         dir_0 = [x[0] for x in table['bi']]
         dir_1 = [x[1] for x in table['bi']]
@@ -48,24 +53,24 @@ def imix_test(server, mult):
         streams = profile.get_streams()
 
         # add both streams to ports
-        c.add_streams(streams, ports=dir_0)
-        c.add_streams(streams, ports=dir_1)
+        client.add_streams(streams, ports=dir_0)
+        client.add_streams(streams, ports=dir_1)
 
         # clear the stats before injecting
-        c.clear_stats()
+        client.clear_stats()
 
         # choose rate and start traffic for 10 seconds
         duration = 10
         print("Injecting {0} <--> {1} on total rate of '{2}' for {3} seconds"
               .format(dir_0, dir_1, mult, duration))
 
-        c.start(ports=(dir_0 + dir_1), mult=mult, duration=duration, total=True)
+        client.start(ports=(dir_0 + dir_1), mult=mult, duration=duration, total=True)
 
         # block until done
-        c.wait_on_traffic(ports=(dir_0 + dir_1))
+        client.wait_on_traffic(ports=(dir_0 + dir_1))
 
         # read the stats after the test
-        stats = c.get_stats()
+        stats = client.get_stats()
 
         # use this for debug info on all the stats
         # pprint(stats)
@@ -87,14 +92,15 @@ def imix_test(server, mult):
         print("\npackets lost from {0} --> {1}:   {2:,} pkts".format(dir_0, dir_0, lost_0))
         print("packets lost from {0} --> {1}:   {2:,} pkts".format(dir_1, dir_1, lost_1))
 
-        if c.get_warnings():
+        if client.get_warnings():
             print("\n\n*** test had warnings ****\n\n")
-            for w in c.get_warnings():
+            for w in client.get_warnings():
                 print(w)
 
-        if (lost_0 <= 0) and (lost_1 <= 0) and not c.get_warnings():    # less or equal because we
-                                                                        #  might have incoming arps
-                                                                        # etc.
+        if (lost_0 <= 0) and (lost_1 <= 0) and not client.get_warnings():   # less or equal
+                                                                            # because we might
+                                                                            # have incoming arps
+                                                                            # etc.
             passed = True
         else:
             passed = False
@@ -105,7 +111,7 @@ def imix_test(server, mult):
         sys.exit(1)
 
     finally:
-        c.disconnect()
+        client.disconnect()
 
     if passed:
         print("\nTest has passed :-)\n")
@@ -127,4 +133,4 @@ parser.add_argument('-m', '--mult',
 args = parser.parse_args()
 
 # run the tests
-imix_test(args.server, args.mult)
+sparta_basic_test(args.server, args.mult)
