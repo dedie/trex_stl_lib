9a30037f49526c73a9277c3ebf13add918fa4092
Daron Andrew Edie <dedie@superloop.com>
2019-01-09 11:34:50 +1000

    Fixing paths

--- examples/stl_path.py
+++ examples/stl_path.py
@@ -3,6 +3,6 @@ import sys
 
 cur_dir = os.path.dirname(__file__)
 
-# Run from TREX_STL_LIB directory
-sys.path.insert(0, "../")
-STL_PROFILES_PATH = '/root/trex_stl_lib/profiles/stl'
+# Run from Trex directory
+sys.path.insert(0, '/opt/trex/v2.48/trex_stl_lib')
+STL_PROFILES_PATH = '/opt/trex/v2.48/trex_stl_lib/profiles/stl'
